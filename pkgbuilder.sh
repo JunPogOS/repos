bdb86430e2fa7e8c956c18b848e7497257f19a84
Ocillacubes <squirtle1607@rollingoak.com>
2022-01-16 15:22:18 -0600

    hmmm

--- pkgbuilder.sh
+++ pkgbuilder.sh
@@ -1,10 +1,21 @@
-cd $2
+if [ $1 -z ]
+then
+	echo "pass the repository name as $1, and the package name as $2"
+	exit
+fi
+if [ $2 -z ]
+then
+	echo "pass the repository name as $1, and the package name as $2"
+	exit
+fi
+
+cd $1
 mkdir building
-cp $1/PKGBUILD building/
+cp $2/PKGBUILD building/
 cd building
 makechrootpkg -u -c -r $CHROOT
 cd ..
 cp building/*.pkg.tar.zst x64_64/
 rm -rf building
 cd x86_64
-repo-add $2.db.tar.gz *.pkg.tar.zst
+repo-add $1.db.tar.gz *.pkg.tar.zst
